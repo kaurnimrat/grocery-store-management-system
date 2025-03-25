48f5ab629ae8c9671944a1805e4c5c82b6aacb71
kaurnimrat <santokhsingh11074@icloud.com>
2025-03-25 14:36:45 +0530

    dashboard

--- grocery_store/staff/urls.py
+++ grocery_store/staff/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("login",views.loginPage),
     path("dashboard",views.dashboardPage),
     path("api/login",views.login_view),
+    path("inventory",views.inventoryPage),
+    path("orders",views .orderPage),
+    path("addstock",views.addStockPage),
 ]
 
--- grocery_store/staff/views.py
+++ grocery_store/staff/views.py
@@ -4,16 +4,59 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt 
 import json
+from .models import *
 
 # Create your views here.
 def landingPage(request):
-    return render(request, "landingPage.html")
+    items_list = Item.objects.all()
+    context = {
+        "products": [],
+        "Category": [],
+    }
+
+    for item in items_list:
+        temp = {
+            "name" : item.name,
+            "price" : item.price,
+        }
+        context["products"].append(temp)
+    
+
+    print(context)
+    return render(request, "landingPage.html", context)
 
 def loginPage(request):
     return render(request, "login.html")
 
 def dashboardPage(request):
-    return render(request, "dashboard.html")
+    context = {
+        'orders' : [],
+    }
+
+    orders = Order.objects.all()
+    print(orders)
+    for order in orders:
+        if (order.is_paid):
+            continue
+        else:
+            temp = {
+                "name": order.name,
+                "items": [],
+            }
+            total = 0
+            for item in order.items.all():
+                temp_item = {
+                    "name" : item.name,
+                    "price": item.price,
+
+                }
+                total += item.price
+                temp["items"].append(temp_item)
+
+            temp["total"]=total
+            context["orders"].append(temp)
+
+    return render(request, "dashboard.html", context)
 
 @csrf_exempt
 def login_view(request):
@@ -29,3 +72,35 @@ def login_view(request):
         else:
             return JsonResponse({"success": False, 'message': 'Invalid credentials'})
     return JsonResponse({'success': False, 'message': 'invalid request method'})
+
+def inventoryPage(request):
+    return render(request,"inventory.html")
+
+def orderPage(request):
+    context = {
+        'orders' : [], 
+    }
+
+    orders = Order.objects.all()
+    print(orders)
+    for order in orders:
+        temp = {
+            "name": order.name,
+            "items": [],
+        }
+        total = 0
+        for item in order.items.all():
+            temp_item = {
+                "name" : item.name,
+                "price": item.price,
+
+            }
+            total += item.price
+            temp["items"].append(temp_item)
+            
+        temp["total"]=total
+        context["orders"].append(temp)
+    return render(request,"order.html", context)
+
+def addStockPage(request):
+    return render(request,"add_stock.html")
