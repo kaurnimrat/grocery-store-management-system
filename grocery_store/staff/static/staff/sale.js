651c3a48c2b6523ddb5836e6834a2b7f3f6e8424
kaurnimrat <santokhsingh11074@icloud.com>
2025-03-27 18:34:42 +0530

    sale

--- grocery_store/staff/static/staff/sale.js
+++ grocery_store/staff/static/staff/sale.js
@@ -1,7 +1,7 @@
 // SIDEBAR TOGGLE
 
 var sidebarOpen = false;
-var sidebar = document.getElementById{"sidebar"};
+var sidebar = document.getElementById("#sidebar");
 
 function openSidebar() {
     if(!sidebarOpen) {
@@ -22,100 +22,6 @@ function closeSidebar() {
 //--------- CHARTS ----------
 
 //BAR CHART
-// var barChartOptions = {
-//     series: [{
-//         data: [10, 8, 6, 4, 2],
-//         name: "Products",
-//     }],
-//     charts: {
-//       type: "bar",
-//       background: "transparent",
-//       height: 350,
-//       toolbar: {
-//         show: false,
-//       },
-//     },
-//     colors: [
-//         "#2962ff",
-//         "#d50000",
-//         "#2e7d32",
-//         "#ff6d00",
-//         "#583cb3",
-//     ],
-//     plotOptions: {
-//         bar: {
-//             distribution: true,
-//             borderRadius: 4,
-//             horizontal: false,
-//             columnWidth: "40%",
-//         }
-//     },
-//     dataLabels: {
-//      enabled: false,
-//     },
-//     fill: {
-//      opacity: 1,
-//     },
-//     grid: {
-//         borderColor: "#55596e",
-//         yaxis: {
-//             lines: {
-//                 show: true,
-//             },
-//         },
-//         xaxis: {
-//          lines: {
-//           show: true,
-//             },
-//         },
-//     },
-//     legend: {
-//         labels: {
-//             colors: "#f5f7ff",
-//         },
-//         show: true,
-//         position: "top",
-//     },
-//     stroke: {
-//         colors: ["tranparent"],
-//         show: true,
-//         width: 2
-//     },
-//     tooltip: {
-//         shared: true,intersect: false,
-//         theme: "dark",
-//     },
-//     xaxis: {
-//         categories: ["Laptop", "Phone", "Monitor", "Headphones", "Camera"],
-//         title: {
-//             style: {
-//                 color: "#f5f7ff",
-//             },
-//         },
-//         axisBorder: {
-//             show: true,
-//             color: "#55596e",
-//         },
-//         axisTricks: {
-//             show: true,
-//             color: "#55596e",
-//         },
-//         labels: {
-//             style: {
-//                 colors: "#f5f7ff",
-//             },
-//         },
-//     },
-//     yaxis: {
-//         title:{
-//             text: "Count",
-//             style: {
-//                 color: "#f5f7ff",
-//             },
-//         },
-//     }
-// },
-
 
 var options = {
     series: [{
@@ -136,11 +42,73 @@ var options = {
     enabled: false
   },
   xaxis: {
-    categories: ['South Korea', 'Canada', 'United Kingdom', 'Netherlands', 'Italy', 'France', 'Japan',
-      'United States', 'China', 'Germany'
+    categories: ['vegetables', 'fruits', 'dairy products', 'other'
     ],
   }
   };
 
+  document .addEventListener("DOMContentLoaded",function() {
+
   var chart = new ApexCharts(document.querySelector("#chart"), options);
   chart.render();
+
+
+// AREA CHART
+ 
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+var options = {
+  series: [{
+  name: 'TEAM A',
+  type: 'area',
+  data: [44, 55, 31, 47, 31, 43, 26, 41, 31, 47, 33]
+}, {
+  name: 'TEAM B',
+  type: 'line',
+  data: [55, 69, 45, 61, 43, 54, 37, 52, 44, 61, 43]
+}],
+  chart: {
+  height: 350,
+  type: 'line',
+},
+stroke: {
+  curve: 'smooth'
+},
+fill: {
+  type:'solid',
+  opacity: [0.35, 1],
+},
+labels: ['Dec 01', 'Dec 02','Dec 03','Dec 04','Dec 05','Dec 06','Dec 07','Dec 08','Dec 09 ','Dec 10','Dec 11'],
+markers: {
+  size: 0
+},
+yaxis: [
+  {
+    title: {
+      text: 'Series A',
+    },
+  },
+  {
+    opposite: true,
+    title: {
+      text: 'Series B',
+    },
+  },
+],
+tooltip: {
+  shared: true,
+  intersect: false,
+  y: {
+    formatter: function (y) {
+      if(typeof y !== "undefined") {
+        return  y.toFixed(0) + " points";
+      }
+      return y;
+    }
+  }
+}
+};
+
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
